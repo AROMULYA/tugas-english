90e06a70c995bc9e99cd8c0fc366f6b7c99043d0
AROMULYA <aropratama55@gmail.com>
2024-10-16 19:07:40 +0700

    Add files via upload

---
+++ beranda.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.querySelector('.tombol-menu');
+    const navMenu = document.querySelector('.menu');
+
+    menuToggle.addEventListener('click', function() {
+        navMenu.classList.toggle('active');
+        menuToggle.classList.toggle('active');
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.querySelector('.tombol-menu');
+    const navMenu = document.querySelector('.menu');
+
+    menuToggle.addEventListener('click', function() {
+        navMenu.classList.toggle('active');
+        menuToggle.classList.toggle('active');
+    });
+});
